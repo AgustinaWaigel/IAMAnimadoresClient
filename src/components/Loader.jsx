17f63e839077175a456975bc4330f1d1ac338dd0
AgustinaWaigel <soledad.waigel@uap.edu.ar>
2025-05-02 21:02:09 -0300

    fd

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ export default function Loader() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
       <img
-        src="jesus con niña.jpg"
+        src="/client/public/gps.jpg"
         alt="Cargando"
         className="w-32 h-32 animate-bounce"
       />
