8150ec71ca3ab5bc9f03154e3b0eafea91afb0cd
AgustinaWaigel <soledad.waigel@uap.edu.ar>
2025-05-22 17:28:39 -0300

    s

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,19 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js")
+      .then((registration) => {
+        console.log("✅ Service Worker registrado:", registration);
+      })
+      .catch((err) => {
+        console.error("❌ Error al registrar Service Worker:", err);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
