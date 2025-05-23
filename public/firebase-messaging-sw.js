7a11e51b459105be08f56c274679684a4ed2de5c
AgustinaWaigel <soledad.waigel@uap.edu.ar>
2025-05-23 09:21:09 -0300

    h

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,14 +12,13 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("📩 Background Message:", payload); // 👈 importante
+  console.log("📩 Notificación en segundo plano:", payload); // 👈 esto
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
     icon: "/logo192.png",
     data: payload.data,
   });
 });
-
 
 self.addEventListener("notificationclick", function(event) {
   event.notification.close();
